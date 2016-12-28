89b8998587093478a9bc15c5ffcdd7b06c9d130c
Edip YILDIZ <edip.yildiz@hotmail.com>
2016-12-28 14:23:36 +0300

    initial commit

---
+++ src/main/java/com/lider/TestController.java
@@ -0,0 +1,17 @@
+package com.lider;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by pardus on 12/20/16.
+ */
+
+@Controller
+
+public class TestController {
+    @RequestMapping("/test")
+       public String test(){
+        return "View";
+    }
+}
